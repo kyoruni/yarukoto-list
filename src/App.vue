ddbf927f217fc3b58bbb7d8dc71381a338008069
kyoruni <kyoruni@gmail.com>
2020-02-07 22:27:25 +0900

    :wrench: ページタイトル修正

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- ヘッダーここから -->
     <b-navbar type="dark" variant="info">
-      <b-navbar-brand href="#">title</b-navbar-brand>
+      <b-navbar-brand href="#">やることリスト</b-navbar-brand>
     </b-navbar>
     <!-- ヘッダーここまで -->
     <router-view/>
